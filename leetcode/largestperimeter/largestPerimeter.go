9730755f6b23d39dba1fe84e20a7a28877b0ff29
liyang <liyang@codoon.com>
2020-07-27 15:28:43 +0800

    init

---
+++ leetcode/largestperimeter/largestPerimeter.go
@@ -0,0 +1,15 @@
+func largestPerimeter(A []int) int {
+    sort.Ints(A)
+    // [3,6,2,55,44,78,65,123]
+    // sort.Sort(sort.Reverse(sort.IntSlice(A)))
+    max := 0
+    for i := len(A) - 1; i >= 2 ; i-- {
+        if A[i - 2] <= A[i] - A[i - 1]{
+            continue
+        }else{
+            max = A[i] + A[i - 1] + A[i - 2]
+            return max
+        }
+    }
+    return max
+}
